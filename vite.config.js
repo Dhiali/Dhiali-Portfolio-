b270ac7999cc5602f548c38ce75ec9ec1cac32bc
Dhiali <231299@virtualwindow.co.za>
2026-01-21 10:27:23 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? './' : '/',
+  base: command === 'build' ? '/Dhiali-Portfolio-/' : '/',
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg', '**/*.webp']
 }))
