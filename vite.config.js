42aae5cf2a63c932f8f51dfb7450729cdc1e52c0
Dhiali <231299@virtualwindow.co.za>
2026-01-30 11:28:11 +0200

    Fix: Update vite.config.js for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Dhiali-Portfolio-',
+  base: '/Dhiali-Portfolio-/',
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg', '**/*.webp'],
   build: {
     outDir: 'dist'
